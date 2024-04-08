608161f6c860474468edfa2943ae8422deb3bb44
Riyaz-commits <skriyazbasha91@gmail.com>
2024-04-08 22:36:52 +0530

    New example changes in feature

--- src/test/java/MyStepdefs.java
+++ src/test/java/MyStepdefs.java
@@ -5,7 +5,10 @@ import static io.restassured.RestAssured.given;
 public class MyStepdefs {
     @Given("I perform the GET Operation for {string}")
 
+
+
     public void iPerformTheGETOperationFor() {
         given().contentType("");
+        System.out.println("Just an example");
     }
 }
